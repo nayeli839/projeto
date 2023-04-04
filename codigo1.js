e16be3c82543a91d184b6888b44ded62eef03557
nayeli839 <129067991+nayeli839@users.noreply.github.com>
2023-04-04 15:06:20 -0300

    Add files via upload
    exercícios básicos de javaScript. Alguns preciso dar uma melhorada...

---
+++ codigo1.js
@@ -0,0 +1,76 @@
+ 
+//for ( let n = 1; n <= 10; n++ ){
+   // console.log(n)
+//}
+
+
+
+//var n= prompt ('numero')
+//var total = n/2;
+
+//if (n & 1){
+  // console.log('impar')
+//} else {
+   // console.log( 'par')
+//}
+ 
+
+
+//var lista = [ 1, 2, 3, 4, 5, 6 ,7];
+//console.log (Math.max(...lista));
+
+
+
+/*var teste = [3, 3, 3, 3];
+var valor = 0;
+for (var s of teste) {
+    if (s === 3) valor++;
+}
+console.log(valor);*/ 
+
+/*var teste = [1, 3, 3, 3];
+var uniqueTeste = [...new Set(teste)]
+console.log(uniqueTeste);*/
+
+
+/*const i = ['oi' , 'tudo bem'];
+i.reverse();
+console.log(i);*/
+
+//function palindrome(str) {
+   // var string= '';
+   // return true;
+// }
+ // palindrome("ana");
+
+ /* var n= prompt ('Digite uma palavra')
+  for (var n = palavra.length - 1; i >= 0; i--) {
+    palindromo += palavra[i]
+ }
+   if (palavra === n ){
+   console.log(' polindromo')
+  } else {
+    console.log( 'nao é um polindrmo')
+}*/
+
+/*var a= prompt ('numero')
+function factorialize (a){
+ 
+    if ( a < 0 ) {
+        return -1;
+    } else if(a == 0) {
+        return 1;
+    } else {
+        (a * factorialize(a - 1));
+    }
+    
+    return factorialize(5);
+}*/
+ 
+
+
+
+ 
+
+
+
